1452186b5e5d0432312dcf03907c921ac2b46b2e
Angelo Pirola <angelo9871@gmail.com>
2022-02-19 16:11:01 +0100

    Aggiunta ClassLibrary BlazorDev.Autentica.Models

---
+++ src/BlazorDev.Autentica.Models/Models/InputModels/RegisterInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDev.Autentica.Models.Models.InputModels
+{
+    public class RegisterInputModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address")]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "La password deve essere lunga tra {2} e {1} caratteri", MinimumLength = 6)]
+        public string Password { get; set; } = String.Empty;
+    }
+}
